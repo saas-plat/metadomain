5039a4f52b1c8fe3a365b9b3a09b136d53b5bd97
caizx <caizx@chanjet.com>
2019-08-12 14:40:08 +0800

    修改查询对象

--- test/MetaTable.test.js
+++ test/MetaTable.test.js
@@ -1,5 +1,5 @@
 const {
-  DataTable,
+  BaseTable,
   MetaTable
 } = require('../lib');
 const {
@@ -9,9 +9,86 @@ const util = require('util');
 
 describe('数据表', () => {
 
+  it('创建一个简单数据表，可以增删改数据', async () => {
+
+    const DataTable1 = MetaTable.create(BaseTable, 'DataTable1', {
+      "id": "string",
+      "Code": "string",
+      "Str1": {
+        type: 'string',
+      },
+      "Date": "date",
+      "Value": {
+        type: 'number',
+      },
+      "Bool1": 'bool', // 布尔
+      "Ref": 'mixed',
+      "Obj1": { // 对象类型
+        "Code": "string",
+        "Name": "string"
+      },
+      'Details': [{ // 子表
+        "Value": "number",
+        "REF2": {
+          "id": "string",
+          "Code": "string",
+          "Name": "string"
+        }
+      }]
+    }, {
+      testid: 'xxxx'
+    }, {
+      on: (...args) => {
+        console.log(args)
+      }
+    });
+
+    const dt1 = new DataTable1({
+      id: 'aaaa',
+      Name: 'test001',
+      Str1: 'abcxyz',
+      Bool1: true,
+      Obj1: {
+        Code: 'eeeeeeeeee'
+      },
+      Ref1: {
+        id: '100'
+      },
+      Details: [{
+        REF2: {
+          id: 'xxxxx'
+        },
+        Value: 100
+      }]
+    });
+    await dt1.save();
+    await DataTable1.commit();
+
+    await DataTable1.findOneAndUpdate({
+      id: 'aaaa'
+    }, {
+      $addToSet: {
+        Details: [{
+          REF2: {
+            id: 'xxxxx'
+          },
+          Value: 200
+        }]
+      }
+    });
+
+    await DataTable1.findOneAndUpdate({
+      id: 'aaaa'
+    }, {
+      id: 'aaaa',
+      Value: 100
+    });
+
+  });
+
   it('只创建一个Schame给gql生成类型用', async () => {
 
-    const DataTable1 = MetaTable.createSchame(  'DataTable1', {
+    const DataTable1 = MetaTable.createSchame(BaseTable, 'DataTable1', {
       "id": "string",
       "Code": "string",
       "Str1": {
@@ -38,72 +115,4 @@ describe('数据表', () => {
     });
 
   })
-
-  it('创建一个简单数据表，可以增删改数据', async () => {
-
-    const DataTable1 = MetaTable.create(DataTable, 'DataTable1', {
-      "id": "string",
-      "Code": "string",
-      "Str1": {
-        type: 'string',
-      },
-      "Date": "date",
-      "Value": {
-        type: 'number',
-      },
-      "Bool1": 'bool', // 布尔
-      "Ref": 'mixed',
-      "Obj1": { // 对象类型
-        "Code": "string",
-        "Name": "string"
-      },
-      'Details': [{ // 子表
-        "Value": "number",
-        "REF2": {
-          "id": "string",
-          "Code": "string",
-          "Name": "string"
-        }
-      }]
-    });
-
-    await test.upsert({
-      id: 'aaaa',
-      Name: 'test001',
-      Str1: 'abcxyz',
-      Bool1: true,
-      Obj1: {
-        Code: 'eeeeeeeeee'
-      },
-      Ref1: {
-        id: '100'
-      },
-      Details: [{
-        REF2: {
-          id: 'xxxxx'
-        },
-        Value: 100
-      }]
-    });
-
-    await test.upsert({
-      {
-        id: 'aaaa',
-        $addToSet: Details: [{
-          REF2: {
-            id: 'xxxxx'
-          },
-          Value: 200
-        }]
-      }
-    });
-
-    await test.upsert({
-      {
-        id: 'aaaa',
-         Value: 100
-      }
-    });
-  });
-
 })
