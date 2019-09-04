ae498926bcf1b220be347a9ace69a50f8d79945d
caizx <caizx@chanjet.com>
2019-09-04 16:55:51 +0800

    测试通过

--- test/Repository.test.js
+++ test/Repository.test.js
@@ -7,8 +7,22 @@ const {
   expect
 } = require('chai');
 const util = require('util');
+const mongo = require('sourced-repo-mongo/mongo');
 
 describe('实体仓库', () => {
+
+  before(async () => {
+    const db = mongo.db;
+    const keys = ['org001.TestObj2', 'org002.TestObj2', 'TestObjBig'];
+    for (const key of keys) {
+      const snapshots = db.collection(key + '.snapshots');
+      const events = db.collection(key + '.events');
+      if (await events.count() > 0) {
+        await events.deleteMany();
+        await snapshots.deleteMany();
+      }
+    }
+  })
 
   it('实体可以设置读取范围，一个组织只能操作一个组织内的实体', async () => {
 
@@ -19,10 +33,10 @@ describe('实体仓库', () => {
 
     // scope 和 schema 字段同名不冲突
     const testRepository1 = await Repository.create(TestObj, {
-      orgid: 'org001'
+      ns: 'org001'
     });
     const testRepository2 = await Repository.create(TestObj, {
-      orgid: 'org002'
+      ns: 'org002'
     });
 
     const test = await TestObj.create();
@@ -31,11 +45,11 @@ describe('实体仓库', () => {
       updateBy: 'aaa',
       ts: test.ts
     });
-    testRepository1.commitAll(test);
+    await testRepository1.commitAll(test);
 
     // 不能读取其他组织的实体
-    expect(testRepository2.get(test.id)).to.be.null;
-    expect(testRepository1.get(test.id)).to.not.null;
+    expect(await testRepository1.get(test.id)).to.not.null;
+    expect(await testRepository2.get(test.id)).to.be.null;
 
     const test2 = await TestObj.create();
     await test2.save({
@@ -44,14 +58,41 @@ describe('实体仓库', () => {
       updateBy: 'aaa',
       ts: test2.ts
     });
-    testRepository2.commitAll(test2);
+    await testRepository2.commitAll(test2);
 
-    expect(testRepository1.get(test2.id)).to.be.null;
-    expect(testRepository2.get(test2.id)).to.not.null;
-    expect(testRepository2.get(test2.id).orgid).to.be.eql('xxxxxx');
+    expect(await testRepository1.get(test2.id)).to.be.null;
+    expect(await testRepository2.get(test2.id)).to.not.null;
+    expect((await testRepository2.get(test2.id)).orgid).to.be.eql('xxxxxx');
   });
 
   it('测试快照保存和加载能力', async () => {
 
+    const TestObj = MetaEntity.create(BaseData, 'TestObjBig', {
+      "orgid": "string",
+      "Code": "string"
+    });
+
+    // scope 和 schema 字段同名不冲突
+    const testRepository = await Repository.create(TestObj, {
+      snapshotFrequency: 5
+    });
+
+    const test = await TestObj.create();
+    for (let i = 0; i < 23; i++) {
+      await test.save({
+        Code: 'CODE_' + i,
+        updateBy: 'aaa',
+        ts: test.ts
+      });
+
+      if (i % 7 == 0) {
+        await testRepository.commitAll(test);
+      }
+    }
+    await testRepository.commitAll(test);
+    expect((await testRepository.get(test.id)).toJS()).to.be.deep.include({
+      Code: 'CODE_' + 22,
+      updateBy: 'aaa'
+    })
   })
 })
