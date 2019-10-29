003fa95a78e992de29c0ce8d11a70105ef6c8dc5
caizx <caizx@chanjet.com>
2019-10-29 15:22:32 +0800

    修改迁移功能，测试通过

--- test/Migration.test.js
+++ test/Migration.test.js
@@ -18,7 +18,7 @@ describe('数据迁移', () => {
 
   before(async () => {
     const db = mongo.db;
-    const keys = ['Department2', 'Warehouse2'];
+    const keys = [ns + '.Department2', ns + '.Warehouse2'];
     for (const key of keys) {
       const snapshots = db.collection(key + '.snapshots');
       const events = db.collection(key + '.events');
@@ -27,7 +27,7 @@ describe('数据迁移', () => {
         await snapshots.deleteMany();
       }
     }
-    const tkeys = ['DataTable2'];
+    const tkeys = [ns + '.DataTable2'];
     for (const key of tkeys) {
       const tables = db.collection(key + '.tables');
       if (await tables.count() > 0) {
@@ -36,76 +36,94 @@ describe('数据迁移', () => {
     }
   })
 
-  const scope = 'test001'
+  const ns = 'test001'
 
   it('实体字段修改，增加、删除、更新类型', async () => {
 
     // 创建初始数据
-    const Department2 = MetaEntity.create(BaseData, "Department2", {
+    const Department1 = MetaEntity.create(BaseData, "Department2", {
       "Code": "string",
       "Name": "string"
     })
-    const Warehouse2 = MetaEntity.create(BaseData, "Warehouse2", {
+    const Warehouse1 = MetaEntity.create(BaseData, "Warehouse2", {
       "Code": "string",
       "Name": "string"
     })
-    const DepartmentRep = await Repository.create(Department2, scope);
-    const WarehouseRep = await Repository.create(Warehouse2, scope);
-    const d = await Department2.create();
+    const Department1Rep = await Repository.create(Department1, {
+      ns
+    });
+    const WarehouseRep = await Repository.create(Warehouse1, {
+      ns
+    });
+    const d = await Department1.create({}, {
+      ns
+    });
     await d.save({
       Code: '111',
       Name: 'aaaaaaaaaa',
       ts: d.ts,
       updateBy: 'ccc'
     });
-    const d2 = await Department2.create();
+    const d2 = await Department1.create({}, {
+      ns
+    });
     await d2.save({
       Code: '222',
       Name: 'bbbbbbbb',
       ts: d2.ts,
       updateBy: 'ccc'
     });
-    const d3 = await Warehouse2.create();
-    await d3.save({
+    const w = await Warehouse1.create({}, {
+      ns
+    });
+    await w.save({
       Code: 'qqqq',
       Name: 'ccccccccc',
-      ts: d3.ts,
+      ts: w.ts,
       updateBy: 'ccc'
     });
-    await DepartmentRep.commitAll(d, d2, d3);
+    await Department1Rep.commitAll(d, d2);
+    await WarehouseRep.commitAll(w);
 
     // 修改schame
-    const Department2_v2 = MetaEntity.create(BaseData, "Department2", {
+    const Department2 = MetaEntity.create(BaseData, "Department2", {
       "Code": "number",
       "Name2": "string"
     }, null, {
       version: 'v2'
     })
+    const Department2Rep = await Repository.create(Department2, {
+      ns
+    });
 
     // 给每个实体的event进行迁移， 每个实体可以写一个升级脚本
     // 通常对一个组织下的所有实体开始升级，升级时需要锁定数据提交
-    const migration = new Migration(Department2_v2, Warehouse2);
-    await db.lock(scope);
+    const migration = new Migration([Department1Rep, WarehouseRep], [Department2Rep, WarehouseRep]);
+    await db.lock(ns);
     await migration.up([`rule update_sciprt1{
       when{
         e: Action e.name == 'Department2.migrate' && e.event == 'saved';
+        d: Event
       }
       then{
-          e.data.Name2 = e.data.Name+'xxxxx';
-          console.log('e.data.Name2 =',e.data.Name2);
+          d.Name2 = e.data.Name+'xxxxx';
+          console.log('d.Name2 =', d.Name2);
       }
     }`]);
-    await db.unlock(scope);
+    await db.unlock(ns);
+    // 只修改了部门的2个save事件
+    expect(migration.upCounter).to.equal(2);
 
     // 检查升级效果
-    const DepartmentRep_v2 = await Repository.create(Department2_v2, scope);
-    const d12 = await DepartmentRep_v2.get(d.id);
+    const d12 = await Department2Rep.get(d.id);
+    console.log(d12)
     expect(d12.Code).to.be.eql(111);
+    expect(d12.Name).to.be.undefined;
     expect(d12.Name2).to.be.eql('aaaaaaaaaaxxxxx');
   })
 
   it('数据表字段修改和数据迁移', async () => {
-    const DataTable = MetaTable.create(BaseTable, 'DataTable2', {
+    const DataTable1 = MetaTable.create(BaseTable, 'DataTable2', {
       "id": "string",
       "Name": "string",
       "Str1": {
@@ -129,9 +147,11 @@ describe('数据迁移', () => {
           "Name": "string"
         }
       }]
+    }, null, {
+      ns
     });
 
-    const dt1 = new DataTable({
+    const dt1 = new DataTable1({
       id: 'aaaa',
       Name: 'test001',
       Str1: 'abcxyz',
@@ -140,6 +160,7 @@ describe('数据迁移', () => {
         Code: 'eeeeeeeeee',
         Name: 'nnnn'
       },
+      Value: 100,
       Ref1: {
         id: '100'
       },
@@ -162,34 +183,36 @@ describe('数据迁移', () => {
         "Code": "string",
         "Name2": "string"
       },
+      "Value": 'string',
       'Details': [{ // 子表
         "REF": {
           "Name": "string"
         }
       }]
     }, null, {
+      ns,
       version: 'v2'
     })
 
-    const migration = new DataMigration(DataTable2);
-    await db.lock(scope);
-    await migration.up(
-      [`rule update_sciprt1{
+    const migration = new DataMigration([DataTable1], [DataTable2]);
+    await db.lock(ns);
+    await migration.up([`rule update_sciprt1{
       when{
         e: Action e.name == 'DataTable2.migrate' ;
         d: Document  ;
       }
       then{
           console.log('===>',e.data);
-          d.Code = e.data.Name;
+          d.Code = e.data.Name ;
       }
     }`]);
-    await db.unlock(scope);
+    await db.unlock(ns);
 
     // 检查升级效果
     const d12 = await DataTable2.findOne({
       id: dt1.id
     });
+    console.log(JSON.stringify(d12.toObject(), null, 1))
     expect(d12.Code).to.be.eql('test001');
     expect(d12.toObject().Details[0]).to.be.eql({
       "REF": {
@@ -216,10 +239,14 @@ describe('数据迁移', () => {
       "Code": "string",
       "Name": "string"
     })
-    const DepartmentRep = await Repository.create(Department1, scope);
+    const Department1Rep = await Repository.create(Department1, {
+      ns
+    });
     const d = await Department1.create({
       Code: '000',
       Name: 'aaaaaaaaaa'
+    }, {
+      ns
     });
     for (let i = 0; i < 20; i++) {
       await d.save({
@@ -228,7 +255,23 @@ describe('数据迁移', () => {
         updateBy: 'ccc'
       });
     }
-    await DepartmentRep.commitAll(d);
+    await Department1Rep.commitAll(d);
+
+    const DataTable1 = MetaTable.create(BaseTable, "DataTable2", {
+      "id": "string",
+      "Code": "string",
+      "Obj1": {
+        "Code": "string",
+        "Name2": "string"
+      },
+      'Details': [{ // 子表
+        "REF": {
+          "Name": "string"
+        }
+      }]
+    }, null, {
+      ns
+    })
 
     // ----------- v2 ---------------
     const Department2 = MetaEntity.create(BaseData, "Department2", {
@@ -237,10 +280,9 @@ describe('数据迁移', () => {
     }, null, {
       version: 'v2'
     })
-    const Warehouse2 = MetaEntity.create(BaseData, "Warehouse2", {
-      "Code": "string",
-      "Name": "string"
-    })
+    const Department2Rep = await Repository.create(Department2, {
+      ns
+    });
 
     const DataTable2 = MetaTable.create(BaseTable, "DataTable2", {
       "id": "string",
@@ -255,10 +297,11 @@ describe('数据迁移', () => {
         }
       }]
     }, null, {
+      ns,
       version: 'v2'
     })
 
-    const migration = new Migration(Department2, Warehouse2);
+    const migration = new Migration([Department1Rep], [Department2Rep]);
     await migration.backup();
     try {
       await migration.up([`rule update_sciprt1{
@@ -274,11 +317,10 @@ describe('数据迁移', () => {
       await migration.rollback();
     }
 
-    const datamigration = new DataMigration(DataTable2);
+    const datamigration = new DataMigration([DataTable1], [DataTable2]);
     await datamigration.backup();
     try {
-      await datamigration.up(
-        [DataTable2], [`rule update_sciprt1{
+      await datamigration.up([`rule update_sciprt1{
       when{
         e: Action e.name == 'DataTable2.migrate';
         d: Document;
@@ -292,8 +334,8 @@ describe('数据迁移', () => {
       await datamigration.rollback();
     }
 
-    const data = await DepartmentRep.getAll();
-    expect(data.length).to.equal(4);
-    expect(data[3].Code).to.equal('0019');
+    const data = await Department1Rep.getAll();
+    expect(data.length).to.equal(3);
+    expect(data.map(it=>it.Code)).to.include('0019');
   })
 })
