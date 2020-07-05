e54d5ba546f660a242f291691dd7402584eb723f
caizx <caizx@saas-plat.com>
2020-07-05 21:54:16 +0800

    fix: 漏提交

--- test/BaseData.test.js
+++ test/BaseData.test.js
@@ -30,7 +30,7 @@ describe('基础数据结构对象', () => {
   it('创建一个仓库档案，增删改仓库基础数据', async () => {
     let eventbus = [];
 
-    const Warehouse = MetaEntity.createModel(BaseData, 'Warehouse3', {
+    const Warehouse = createModel(BaseData, 'Warehouse3', {
 
       "Code": {
         type: "string",
@@ -219,11 +219,11 @@ describe('基础数据结构对象', () => {
 
   it('创建一个仓库档案，并引用了人员和部门档案等信息', async () => {
     let eventbus = [];
-    const Department = MetaEntity.createModel(BaseData, "Department3", {
+    const Department = createModel(BaseData, "Department3", {
       "Code": "string",
       "Name": "string"
     })
-    const Persion = MetaEntity.createModel(BaseData, 'Persion3', {
+    const Persion = createModel(BaseData, 'Persion3', {
       "Code": "string",
       "Status": 'bool',
       "Name": "string",
@@ -255,11 +255,11 @@ describe('基础数据结构对象', () => {
       "PostCode": "string",
       "PostAddr": "string"
     });
-    const WarehouseType = MetaEntity.createModel(BaseData, 'WarehouseType3', {
+    const WarehouseType = createModel(BaseData, 'WarehouseType3', {
       "Code": "string",
       "Name": "string"
     });
-    const Warehouse = MetaEntity.createModel(BaseData, 'Warehouse4', {
+    const Warehouse = createModel(BaseData, 'Warehouse4', {
       "Code": {
         type: "string",
         required: true // 必录
